b855abbcd696714b3ab9c7fbb77277bb1c17d8cc
ABA11 <abermudeza@ucatolica.ac.cr>
2016-11-23 21:14:30 -0600

    Proyecto Pogra ll
    Avance uno

---
+++ Proyecto_Progra ll/src/clasesP/Articulo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasesP;
+
+/**
+ *
+ * @author Andrey B
+ */
+public class Articulo {
+    String nombreA;
+    String estado;
+    String color;
+    String precio;
+}
